b05196a8f0a401edf95a569857d218a7427f70a8
sarah <clergentsarah@gmail.com>
2017-07-16 13:27:42 -0400

    creation hash =

--- mairie.rb
+++ mairie.rb
@@ -5,25 +5,30 @@ require 'nokogiri'
 require 'open-uri'
 
 
-def get_the_email_of_a_townhal_from_its_webpage 
-    doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
-    puts doc.xpath("//tr//td//p[@class = 'Style22']//font")[11]
+def get_the_email_of_a_townhal_from_its_webpage(name, href)
+    doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com" + href[1..-1]))
+    res = Hash[
+        "name" => name,
+        "email" => doc.xpath("//tr//td//p[@class = 'Style22']//font")[11].to_s[16..-8] 
+    ]
+    
+    puts res
+    return res
 end
-get_the_email_of_a_townhal_from_its_webpage()
 
 
 def get_all_the_urls_of_val_doise_townhalls()
     doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 
+    result = Array.new
     doc.xpath("//table[@class = 'Style20']//a").each do |link| 
         #puts link.to_html
-        h = {}
         res = link.to_html.match(/href="(.+)">(.+)<\/a>/)
         if res then
-            h.store("href", res[1])
-            h.store("name", res[2])
-            puts h 
+            result.push get_the_email_of_a_townhal_from_its_webpage(res[2], res[1]) 
         end
     end
+    return result
 end
 
+get_all_the_urls_of_val_doise_townhalls()
